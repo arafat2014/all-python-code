832917ba29c99e6de9c93f6b9687ca33f73253ea
arafat2014 <arafatsharif7890@gmail.com>
2025-01-27 00:02:53 +0600

    Lamda Funcation

--- prectice.py
+++ prectice.py
@@ -1,5 +1,9 @@
-a = 4
-b = "4"
-
-print(a is b)
-print(a == b)
+def apply(fx, value):
+  return 6 + fx(value)
+dauble = lambda x: x*2
+cube = lambda x: x*x*x
+avg = lambda x,y: (x+y)/2
+print(dauble(5))
+print(cube(5))
+print(avg(3,5))
+print(apply(cube, 2))
