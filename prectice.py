d7c1e0632178ff7d51c6b2181bd94eb5a56ae945
arafat2014 <arafatsharif7890@gmail.com>
2025-01-27 18:16:34 +0600

    OOP Cass

--- prectice.py
+++ prectice.py
@@ -1,15 +1,22 @@
 class person:
   name = "Arafat"
   occupation = "Software Engineer"
+  age = 25
+  city = "Dhaka"
   def info(self):
-    print(f"{self.name} is a {self.occupation}")
+    print(f"{self.name} is a {self.occupation} his age is {self.age} he lives is {self.city}")
 
 a = person()
 b = person()
 a.name = "Anamul"
 a.occupation = "Web developer"
-a.name = "Anamul"
-a.occupation = "Web developer"
+a.age = 26
+a.city = "Tangail"
+
+b.name = "Yasin"
+b.occupation = "App developer"
+b.age = 24
+b.city = "Chadpur"
 
 a.info()
 b.info()
