362c6dc2cd193e2db2e8c8e350cf9006ae37e825
arafat2014 <arafatsharif7890@gmail.com>
2025-01-27 18:12:17 +0600

    OOP class

--- prectice.py
+++ prectice.py
@@ -1,9 +1,15 @@
-def apply(fx, value):
-  return 6 + fx(value)
-dauble = lambda x: x*2
-cube = lambda x: x*x*x
-avg = lambda x,y: (x+y)/2
-print(dauble(5))
-print(cube(5))
-print(avg(3,5))
-print(apply(cube, 2))
+class person:
+  name = "Arafat"
+  occupation = "Software Engineer"
+  def info(self):
+    print(f"{self.name} is a {self.occupation}")
+
+a = person()
+b = person()
+a.name = "Anamul"
+a.occupation = "Web developer"
+a.name = "Anamul"
+a.occupation = "Web developer"
+
+a.info()
+b.info()
