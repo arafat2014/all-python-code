0a3d04644db3e58645dd132ec39e80c5d42b9c80
arafat2014 <arafatsharif7890@gmail.com>
2025-01-27 20:16:09 +0600

    greet

--- prectice.py
+++ prectice.py
@@ -1,13 +1,16 @@
-import logging
+def greet(fx):
+  def mfx():
+    print("Good Morning")
+    fx()
+    print("Thanks for using this fincation")
+  return mfx
+@greet
+def hello():
+    print("Hello world")
+def add(a, b):
+  print(a+b)
+  
+hello()
+add(2,3)
 
-def log_funcation_call(func):
-  def decorated(*args, **kwargs):
-    logging.info(f"calling {func.__name__} with arge={args}, kwargs={kwargs}")
-    result = func(*args, **kwargs)
-    logging.info(f"{func.__name__} returned {result}")
-    return result
-  return decorated
-@log_funcation_call
-def my_funcation(a, b):
-  return a + b
-    
+       
