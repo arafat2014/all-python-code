3af79180e0baa374e0210046fcb99440173ec5b4
arafat2014 <arafatsharif7890@gmail.com>
2025-01-27 23:23:12 +0600

    Getters in python

--- prectice.py
+++ prectice.py
@@ -1,15 +1,12 @@
-def asset(fx):
-  def mfx(*args, **kwargs):
-    print("Good Morning")
-    fx(*args, **kwargs)
-    print("Thanks for using this fincation")
-  return mfx
-@asset
-def hello():
-    print("Hello world")
-@asset
-def add(a, b):
-  print(a+b)
-  
-hello()
-add(1,3)
+class Myclass:
+  def __init__(self, value):
+    self._value = value
+  def show(self):
+    print(f"Value is {self._value}")
+    
+  @property
+  def value(self):
+    return self._value
+obj = Myclass(10)
+obj.show()
+    
