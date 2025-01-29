48eab11663889ee0b2283f4098819a15d8ec9da5
arafat2014 <arafatsharif7890@gmail.com>
2025-01-29 23:05:15 +0600

    Getter and setter

--- prectice.py
+++ prectice.py
@@ -1,10 +1,20 @@
 class MyClass:
     def __init__(self, value):
         self._value = value
+        
     def show(self):
-      print(f"value is {self._value}")   
+      print(f"value is {self._value}")  
+       
     @property
-    def value(self):
+    def ten_value(self):
         return self._value
+    
+    @ten_value.setter
+    def ten_value(self, new_value):
+        self._value = new_value/10
+        
+    
 obj = MyClass(10)
+obj.ten_value = 67
+print(obj.ten_value)
 obj.show()
