152e8eed0be707620ce8beb98027fd5b6d451927
arafat2014 <arafatsharif7890@gmail.com>
2025-02-01 18:12:19 +0600

    Access Modifier

--- prectice.py
+++ prectice.py
@@ -1,22 +1,6 @@
 class Employ:
-    def __init__(self, name, ID):
-        self.name = name
-        self.ID = ID
-    
-    def ShowDetails(self):
-        print(f"The name of employ {self.name} and ID is {self.ID} ")
-        
-class progmeer(Employ):
-    def __init__(self, language):
-        self.language = language
-        
-    def ShowLanguage(self):
-        print(f"The employ work om {self.language} language")
-        
-    
-e = Employ("Sharif Arafat", 25)
-e.ShowDetails()
-b = progmeer("python")
-b.ShowLanguage()
+    def __init__(self):
+        self.name = "Arafat"
 
-   
+e = Employ()
+print(e.name)
