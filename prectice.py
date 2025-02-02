6f797f8d4c683527752d463bd3b6ed700d01a9ed
arafat2014 <arafatsharif7890@gmail.com>
2025-02-03 00:31:12 +0600

    Instance variables vs Class variables in Python

--- prectice.py
+++ prectice.py
@@ -1,17 +1,21 @@
 class Employee:
     companyName ="Apple"
     degignation = "Software Engineer"
+    noOfEmploy = 0
     def __init__(self, name):
         self.name = name
         self.raise_amount = 0.02
+        Employee.noOfEmploy +=1
         
     def showDetails(self):
-        print(f"The Employ name is {self.name} and raise amount in {self.companyName} is {self.raise_amount} and job positiopn is {self.degignation}")
+        print(f"The Employ name is {self.name} and the raise amount in {self.noOfEmploy} sized {self.companyName} is {self.raise_amount} and job positiopn is {self.degignation}")
     
 e1 = Employee("Arafat")
 e1.raise_amount = 2.3
+e1.companyName ="Apple USA"
 e1.showDetails()
 e2 = Employee("Shifa")
+e2.companyName ="Apple UK"
 e2.showDetails()
     
 
